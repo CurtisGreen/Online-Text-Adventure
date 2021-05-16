3c08866a16e18117ba548a8875982e6caf6e9fea
CurtisGreen <c.green5201@gmail.com>
2021-05-16 18:39:37 -0500

    Added stats class and temp fighting logic to test it out (#3)

--- api/Parser.py
+++ api/Parser.py
@@ -1,5 +1,9 @@
+from Player import Player
+from Enemy import Enemy
+
 class Parser():
     def parse(self, input):
+        print('parsing')
         inputArr = self.cleanInput(input)
 
         response = self.checkMove(inputArr)
@@ -32,7 +36,7 @@ class Parser():
             return None
 
     def verifyMove(self, action):
-        actions = {'move', 'go'}
+        actions = {'move', 'go', 'run'}
         return (action in actions)
     
     def verifyDirection(self, dir):
@@ -44,9 +48,49 @@ class Parser():
         actions = {'attack', 'hit', 'kill'}
         # 1 word action ('attack'), attack current target if it exists
         if len(inputArr) == 1 and inputArr[0] in actions:
-            return 'There is nothing to attack here'
+            return self.fightEnemy()
         # 2 word action ('attack goblin'), attack if that target exists
         elif len(inputArr) == 2 and inputArr[0] in actions:
             return 'There is no ' + inputArr[1] + ' to attack here'
         else:
             return None
+
+    # Temp fighting logic
+    def fightEnemy(self):
+        outString = ''
+        player = Player()
+        enemy = Enemy('goblin')
+
+        # Player's turn
+        # Check enemy dodge
+        enemyDodge = enemy.stats.rollDodge(player.stats['agility'])
+        if enemyDodge:
+            outString += 'The enemy dodged.\n'
+        # Do damage
+        else:
+            damage = player.stats.rollAttack()
+            enemy.stats['health'] -= damage
+
+            # Check if enemy is dead
+            if enemy.stats['health'] <= 0:
+                return 'You did ' + str(damage) + ' damage and killed the enemy.'
+            else:
+                outString += 'You did ' + str(damage) + ' damage.\nThe enemy has ' + str(enemy.stats['health']) + ' health left.'
+
+        # Enemy's turn
+        # Check player dodge
+        playerDodge = player.stats.rollDodge(enemy.stats['agility'])
+        if playerDodge:
+            outString += 'You dodged.\n'
+        # Do damage
+        else:
+            damage = enemy.stats.rollAttack()
+            player.stats['health'] -= damage
+
+            # Check if the player is dead
+            if player.stats['health'] <= 0:
+                return 'Enemy did ' + str(damage) + ' damage and killed you. Game over.'
+            else:
+                outString += 'Enemy did ' + str(damage) + ' damage to you.\nYou have ' + str(player.stats['health']) + ' health left.'
+
+        return outString
---
+++ api/Player.py
@@ -0,0 +1,5 @@
+from Stats import Stats
+
+class Player:
+    def __init__(self):
+        self.stats = Stats('playerStats')
